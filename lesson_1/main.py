2eac47d011c25ed1ed07bf0db1d365a2e039e487
Evgeniy <benyahitr@gmail.com>
2022-04-02 17:19:17 +0700

    Переписаны функции print_urls, write_urls_to_file, так чтобы не нарушать принцип SRP. Доавблена валидация url

--- lesson_1/main.py
+++ lesson_1/main.py
@@ -6,38 +6,56 @@ TARGET_SITE_URL = 'https://www.python.org/'
 FILE_NAME = 'output.txt'
 
 
-def get_urls(url: str, limit=10) -> List:  # Чтобы необходить все ссылки на сайте добавил лимит количества ссылок
+def format_url(url: str) -> str:
+    if url[0] == '/':
+        return f'{TARGET_SITE_URL}{url[1:]}' if len(url) > 1 else TARGET_SITE_URL
+    else:
+        return url
+
+
+def url_is_valid(url: str) -> bool:
+    if '#' in url:
+        return False
+    if 'tel:' in url:
+        return False
+    if 'mailto:' in url:
+        return False
+    if 'javascript:' in url:
+        return False
+    if '/' == url:
+        return False
+    return True
+
+
+def get_urls(url: str, sublink=True, limit=20) -> List:  # Чтобы необходить все ссылки на сайте добавил лимит количества ссылок
     urls = []
     try:
         response = requests.get(url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
             a_tags = soup.find_all("a")
-            urls = [item.get('href') for item in a_tags if item.get('href') and 'http' in item.get(
-                'href')]  # отфильтровываем локальные ссылки не содержащие протокол
-    except:
-        print('Network error')
+            urls = [format_url(item.get('href')) for item in a_tags if
+                    item.get('href') and url_is_valid(item.get('href'))][:limit]
+            if sublink:
+                urls = [{url_: get_urls(url_, sublink=False, limit=10)} for url_ in urls][:limit]
+    except ConnectionError:
+        print(f'Connection error')
     finally:
-        return urls[:limit]
+        return urls
 
 
 def print_urls(urls: List):
     for url in urls:
-        print(url)
-        inner_urls = get_urls(url)
-        for inner_url in inner_urls:
-            print(
-                f'####{inner_url}')  # знаки решеток добавил чтобы визуально выделить ссылки которые вложены в первый уровень
+        for key, value in url.items():
+            print(f'{key} ---> {value}')
 
 
 def write_urls_to_file(urls: List, file_name: str):
     try:
         with open(file_name, 'w') as file:
             for url in urls:
-                file.write(f'{url}\n')
-                inner_urls = get_urls(url)
-                for inner_url in inner_urls:
-                    file.write(f'####{inner_url}\n')
+                for key, value in url.items():
+                    file.write(f'{key} ---> {value}\n')
     except:
         print('Error writing to file')
 
@@ -45,12 +63,11 @@ def write_urls_to_file(urls: List, file_name: str):
 def parse_site(output='console'):
     urls = get_urls(TARGET_SITE_URL)
     if output == 'console':
-        # print_urls(urls)
-        pass
+        print_urls(urls)
     elif output == 'file':
         write_urls_to_file(urls, FILE_NAME)
 
 
 if __name__ == '__main__':
-    # parse_site()
+    parse_site()
     parse_site('file')
